ef90d78d159d7f2b8d87bc7f30765cb0bb808282
ygl <ygl-rg@roundgis.com>
2018-01-02 18:01:49 +0800

    update js

--- allin1.profile.js
+++ allin1.profile.js
@@ -45,7 +45,7 @@ var layer2 = ["dojo/dojo", "dojo/i18n", "dojo/domReady",
     "dojox/charting/action2d/Tooltip", "dojox/charting/action2d/MouseZoomAndPan",
     "dojox/charting/action2d/TouchZoomAndPan", "dojox/charting/action2d/TouchIndicator",
     "dojox/charting/plot2d/Markers", "dojox/charting/axis2d/Default", "dojo/fx/easing",
-    "dojox/charting/SimpleTheme"
+    "dojox/charting/SimpleTheme", "dojox/gfx/path", "dojox/gfx/svg"
 ];
 
 var layer3 = ["dojo/dojo", "dojo/i18n", "dojo/domReady",
@@ -73,7 +73,7 @@ var layer3 = ["dojo/dojo", "dojo/i18n", "dojo/domReady",
     "dojox/charting/action2d/Tooltip", "dojox/charting/action2d/MouseZoomAndPan",
     "dojox/charting/action2d/TouchZoomAndPan", "dojox/charting/action2d/TouchIndicator",
     "dojox/charting/plot2d/Markers", "dojox/charting/axis2d/Default", "dojo/fx/easing",
-    "dojox/charting/SimpleTheme",
+    "dojox/charting/SimpleTheme", "dojox/gfx/path", "dojox/gfx/svg",
     "dojox/mobile", "dojox/mobile/compat", "dojox/mobile/RoundRect",
     "dojox/mobile/Button", "dojox/mobile/ValuePickerSlot", "dojox/mobile/ValuePicker",
     "dojox/mobile/Heading", "dojox/mobile/ToolBarButton", "dojox/mobile/View"
@@ -100,6 +100,38 @@ var profile = (function () {
                 "config-selectorEngine": "lite"
             },
             async: 1
+        },
+
+        staticHasFeatures: {
+            "config-deferredInstrumentation": 0,
+            "config-dojo-loader-catches": 0,
+            "config-tlmSiblingOfDojo": 0,
+            "dojo-amd-factory-scan": 0,
+            "dojo-combo-api": 0,
+            "dojo-config-api": 1,
+            "dojo-config-require": 0,
+            "dojo-debug-messages": 0,
+            "dojo-dom-ready-api": 1,
+            "dojo-firebug": 0,
+            "dojo-guarantee-console": 1,
+            "dojo-has-api": 1,
+            "dojo-inject-api": 1,
+            "dojo-loader": 1,
+            "dojo-log-api": 0,
+            "dojo-modulePaths": 0,
+            "dojo-moduleUrl": 0,
+            "dojo-publish-privates": 0,
+            "dojo-requirejs-api": 0,
+            "dojo-sniff": 1,
+            "dojo-sync-loader": 0,
+            "dojo-test-sniff": 0,
+            "dojo-timeout-api": 0,
+            "dojo-trace-api": 0,
+            "dojo-undef-api": 0,
+            "dojo-v1x-i18n-Api": 1,
+            "dom": 1,
+            "host-browser": 1,
+            "extend-dojo": 1
         },
 
         packages: [
